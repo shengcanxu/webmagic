cc00e903322fc6881218d2c8019809bd1104ea6b
cano <xch_554@163.com>
2015-02-03 00:33:51 +0800

    add resetdb annotation

---
+++ webmagic-extension/src/main/java/us/codecraft/webmagic/model/annotation/ResetDB.java
@@ -0,0 +1,22 @@
+package us.codecraft.webmagic.model.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Define the extractor for field or class.<br>
+ *
+ * @author code4crafter@gmail.com <br>
+ * @since 0.2.0
+ */
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface ResetDB {
+
+    /**
+     * true to re-create the table or false for not to
+     * @return
+     */
+    boolean value() default false;
+}
