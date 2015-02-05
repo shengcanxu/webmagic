af4e65557a5ab8ce8eddf68be468f3c0d65aaea4
Cano <xch_554@163.com>
2015-02-05 13:50:14 +0800

    add multiple page support(not complete)

---
+++ webmagic-extension/src/main/java/us/codecraft/webmagic/example/News163Article.java
@@ -0,0 +1,29 @@
+package us.codecraft.webmagic.example;
+
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.model.OOSpider;
+import us.codecraft.webmagic.model.annotation.ExtractBy;
+import us.codecraft.webmagic.model.annotation.ExtractByUrl;
+import us.codecraft.webmagic.model.annotation.ParseUrl;
+import us.codecraft.webmagic.model.annotation.ResetDB;
+import us.codecraft.webmagic.pipeline.ConsolePageModelPipeline;
+import us.codecraft.webmagic.scheduler.StackScheduler;
+
+/**
+ * Created by canoxu on 2015/2/1.
+ */
+
+public class News163Article {
+    @ExtractBy("//div[@id=\"endText\"]")
+    private String content;
+
+    @ExtractByUrl(value = "")
+    private String PageURL;
+
+    public static void main(String[] args) {
+        Site site = Site.me().setTimeOut(10000).setRetryTimes(5).setDomain("163.com");
+        OOSpider.create(site, new ConsolePageModelPipeline(), News163Article.class)
+                .scheduler(new StackScheduler())
+                .addUrl("http://news.163.com/13/0802/05/958I1E330001124J.html").thread(1).run();
+    }
+}
