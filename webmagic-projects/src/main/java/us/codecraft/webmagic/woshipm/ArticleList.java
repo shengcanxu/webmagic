7a5f54ff73cb2e2901fe8d3cd2a71b100e9a36ac
Cano <xch_554@163.com>
2015-02-27 13:48:00 +0800

    articleList changes

--- webmagic-projects/src/main/java/us/codecraft/webmagic/woshipm/ArticleList.java
+++ webmagic-projects/src/main/java/us/codecraft/webmagic/woshipm/ArticleList.java
@@ -3,11 +3,18 @@ package us.codecraft.webmagic.woshipm;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.model.annotation.ExtractBy;
 import us.codecraft.webmagic.model.annotation.ExtractByUrl;
+import us.codecraft.webmagic.model.annotation.Formatter;
 import us.codecraft.webmagic.modelSpider.ModelSpider;
 import us.codecraft.webmagic.modelSpider.PageModel;
+import us.codecraft.webmagic.modelSpider.annotation.ExtractByParseUrl;
+import us.codecraft.webmagic.modelSpider.annotation.FieldType;
 import us.codecraft.webmagic.modelSpider.annotation.ParseUrl;
+import us.codecraft.webmagic.modelSpider.annotation.TextFormatter;
+import us.codecraft.webmagic.modelSpider.pipeline.MysqlPipeline;
 import us.codecraft.webmagic.pipeline.ConsolePipeline;
 import us.codecraft.webmagic.scheduler.RedisScheduler;
+
+import java.util.List;
 
 /**
  * Created by cano on 2015/2/19.
@@ -17,12 +24,14 @@ import us.codecraft.webmagic.scheduler.RedisScheduler;
         subXpath = "//h2/a/@href")
 public class ArticleList extends PageModel {
 
-//    @ExtractByParseUrl(xpath = "",depth = 1)
-//    private String tags;
-//
-//    private String readNum;
-//
-//    private String summary;
+    @ExtractByParseUrl(xpath = "//span[@class=\"f_c_tag\"]",depth = 1)
+    private String tags;
+
+    @ExtractByParseUrl(xpath = "//span[@class=\"f_c_view\"]/i/text()",depth = 1)
+    private String readNum;
+
+    @ExtractByParseUrl(xpath = "//p[@class=\"f_content\"]/text()", depth = 1)
+    private String summary;
 
     @ExtractBy(value = "//h1/text()")
     private String title;
@@ -39,32 +48,34 @@ public class ArticleList extends PageModel {
     @ExtractBy(value = "//div[@class=\"content_box\"]/div[@class=\"con_txt clx\"]/blockquote/p/text()")
     private String daodu;
 
-//    @ExtractBy(value = "//div[@class=\"content_box\"]/div[@class=\"con_txt clx\"]")
-//    private String content;
+    @ExtractBy(value = "//div[@class=\"content_box\"]/div[@class=\"con_txt clx\"]")
+    @FieldType(type = FieldType.Type.TEXT)
+    private String content;
 
-
+    @ExtractBy(value = "//div[@class=\"content_box\"]/div[@class=\"con_txt clx\"]/div[@class=\"ludouVA\"]//div[@class=\"option hxItem\"]/div[@class=\"count\"]/text()")
     private String haixing;
 
+    @ExtractBy(value = "//div[@class=\"content_box\"]/div[@class=\"con_txt clx\"]/div[@class=\"ludouVA\"]//div[@class=\"option hhkItem\"]/div[@class=\"count\"]/text()")
     private String haokan;
 
+    @ExtractBy(value = "//div[@class=\"content_box\"]/div[@class=\"con_txt clx\"]/div[@class=\"ludouVA\"]//div[@class=\"option bhkItem\"]/div[@class=\"count\"]/text()")
     private String buhaokan;
-
-    private String comment;
 
     @ExtractByUrl(regrex = "")
     private String pageUrl;
 
 
     public static void main(String[] args){
-        Site site = Site.me().setRetryTimes(5).setTimeOut(10000).setDomain("woshipm.com").addHeader("Referer","http://www.woshipm.com/");
+        Site site = Site.me().setRetryTimes(5).setTimeOut(100000).setDomain("woshipm.com").addHeader("Referer","http://www.woshipm.com/");
         System.out.println(site);
 
         ModelSpider modelSpider = ModelSpider.create(site, new ArticleList());
-        modelSpider.scheduler(new RedisScheduler("127.0.0.1").setStartOver(true))
+        modelSpider.scheduler(new RedisScheduler("127.0.0.1").setStartOver(false))
+                .addPipeline(new MysqlPipeline())
                 .addPipeline(new ConsolePipeline());
-        for(int i=1; i<=1; i++) {
+        for(int i=1; i<=833; i++) {
             modelSpider.addUrlPost("http://www.woshipm.com/page/" + i);
         }
-        modelSpider.thread(1).run();
+        modelSpider.thread(10).run();
     }
 }
