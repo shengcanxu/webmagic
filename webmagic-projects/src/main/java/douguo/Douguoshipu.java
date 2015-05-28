5c0d093701e772bc161b6890af2e847c0b869173
cano <xch_554@163.com>
2015-05-28 08:08:37 +0800

    add douguomeishi

---
+++ webmagic-projects/src/main/java/douguo/Douguoshipu.java
@@ -0,0 +1,35 @@
+package douguo;
+
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.model.annotation.ExtractByUrl;
+import us.codecraft.webmagic.modelSpider.ModelSpider;
+import us.codecraft.webmagic.modelSpider.PageModel;
+import us.codecraft.webmagic.modelSpider.annotation.ParseUrl;
+import us.codecraft.webmagic.modelSpider.pipeline.ConsoleModelSpiderPipeline;
+
+/**
+ * Created by cano on 2015/5/28.
+ */
+
+@ParseUrl(expression = "//*[@id=\"main\"]/div[@class=\"sortf\"]//ul/li/a/@href")
+@ParseUrl(expression = "//*[@id=\"container\"]//h3/a/@href", nextPageRegion = "//*[@id=\"main\"]//div[@class=\"pagination\"]/span/a/")
+public class Douguoshipu extends PageModel {
+
+
+    @ExtractByUrl(regrex = "")
+    private String pageUrl;
+
+    public static void main(String[] args){
+        Site site = Site.me().setRetryTimes(5).setTimeOut(10000).setCycleRetryTimes(5)
+                .setDomain("douguo.com/").addHeader("Referer","http://www.douguo.com/");
+        System.out.println(site);
+
+        ModelSpider modelSpider = ModelSpider.create(site, new Douguoshipu());
+        modelSpider//.scheduler(new RedisScheduler("127.0.0.1").setStartOver(true))
+                //.addPipeline(new MysqlPipeline().setShouldResetDb(true))
+                .addPipeline(new ConsoleModelSpiderPipeline());
+        modelSpider.addUrl("http://www.douguo.com/caipu/fenlei");
+
+        modelSpider.thread(1).run();
+    }
+}
