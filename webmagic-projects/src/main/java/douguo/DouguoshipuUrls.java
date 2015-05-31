e79b22ced9563519142ebacde2fda3c56bff606f
cano <xch_554@163.com>
2015-05-31 10:53:58 +0800

    增加从文件逐行获取url操作

--- webmagic-projects/src/main/java/douguo/DouguoshipuUrls.java
+++ webmagic-projects/src/main/java/douguo/DouguoshipuUrls.java
@@ -1,36 +1,39 @@
 package douguo;
 
-import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.model.annotation.ExtractByUrl;
-import us.codecraft.webmagic.modelSpider.ModelSpider;
 import us.codecraft.webmagic.modelSpider.PageModel;
 import us.codecraft.webmagic.modelSpider.annotation.ParseUrl;
-import us.codecraft.webmagic.modelSpider.pipeline.ConsoleModelSpiderPipeline;
-import us.codecraft.webmagic.scheduler.RedisScheduler;
+import us.codecraft.webmagic.utils.FileUtils;
+
+import java.util.List;
 
 /**
  * Created by cano on 2015/5/28.
  */
 
-@ParseUrl(expression = "//*[@id=\"main\"]/div[@class=\"sortf\"]//ul/li/a/@href", nextPageRegion = "")
-@ParseUrl(expression = "//*[@id=\"container\"]//h3/a/@href", nextPageRegion = "//*[@id=\"main\"]//div[@class=\"pagination\"]//span/")
+@ParseUrl(expression = "//*[@id=\"main\"]/div[@class=\"sortf\"]//ul/li/a/@href")
+//@ParseUrl(expression = "//*[@id=\"container\"]//h3/a/@href", nextPageRegion = "//*[@id=\"main\"]//div[@class=\"pagination\"]//span/")
 public class DouguoshipuUrls extends PageModel {
 
 
     @ExtractByUrl(regrex = "")
     private String pageUrl;
 
+
+
     public static void main(String[] args){
-        Site site = Site.me().setRetryTimes(5).setTimeOut(10000).setCycleRetryTimes(5).setDeepFirst(true).setMaxDeep(2)
-                .setDomain("douguo.com").addHeader("Referer","http://www.douguo.com/");
-        System.out.println(site);
+//        Site site = Site.me().setRetryTimes(5).setTimeOut(10000).setCycleRetryTimes(5).setDeepFirst(true).setMaxDeep(2)
+//                .setDomain("douguo.com").addHeader("Referer","http://www.douguo.com/");
+//        System.out.println(site);
+//
+//        ModelSpider modelSpider = ModelSpider.create(site, new DouguoshipuUrls());
+//        modelSpider.scheduler(new RedisScheduler("127.0.0.1",site).setStartOver(true))
+//                .addPipeline(new MysqlPipeline().setShouldResetDb(true))
+//                .addPipeline(new ConsoleModelSpiderPipeline());
+//        modelSpider.addUrl("http://www.douguo.com/caipu/fenlei");
+//
+//        modelSpider.thread(1).run();
 
-        ModelSpider modelSpider = ModelSpider.create(site, new DouguoshipuUrls());
-        modelSpider.scheduler(new RedisScheduler("127.0.0.1",site).setStartOver(false))
-                //.addPipeline(new MysqlPipeline().setShouldResetDb(true))
-                .addPipeline(new ConsoleModelSpiderPipeline());
-        modelSpider.addUrl("http://www.douguo.com/caipu/fenlei");
-
-        modelSpider.thread(20).run();
+        List<String> urls = FileUtils.getUrlsFromFile("D:\\software\\redis\\data\\douguocaipumulu.txt");
     }
 }
