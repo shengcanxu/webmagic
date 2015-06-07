abe8f7013a95eb3817a4fedcd27a5747d87036e3
cano <xch_554@163.com>
2015-06-08 07:51:59 +0800

    增加拷贝redis queue的功能

--- webmagic-projects/src/main/java/douguo/Test.java
+++ webmagic-projects/src/main/java/douguo/Test.java
@@ -1,10 +1,6 @@
 package douguo;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import us.codecraft.xsoup.Xsoup;
-
-import java.util.List;
+import us.codecraft.webmagic.utils.redis.RedisCopy;
 
 /**
  * Created by cano on 2015/5/31.
@@ -12,17 +8,21 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args){
-        String html = "<div><a href='https://github.com'>github.com</a></div>" +
-                "<table><tr><td>a</td><td>b</td></tr></table>";
+//        String html = "<div><a href='https://github.com'>github.com</a></div>" +
+//                "<table><tr><td>a</td><td>b</td></tr></table>";
+//
+//        Document document = Jsoup.parse(html);
+//
+//        String result = Xsoup.compile("//a/@href").evaluate(document).get();
+//        //Assert.assertEquals("https://github.com", result);
+//
+//        List<String> list = Xsoup.compile("//tr/td/text()").evaluate(document).list();
+//       // Assert.assertEquals("a", list.get(0));
+//        //Assert.assertEquals("b", list.get(1));
+//        int a = 2;
 
-        Document document = Jsoup.parse(html);
+        RedisCopy copy = RedisCopy.getInstance();
+        copy.copyQueue("queue_dupicate_ChufangCaipuUrls","queue_ChufangCaipuUrls");
 
-        String result = Xsoup.compile("//a/@href").evaluate(document).get();
-        //Assert.assertEquals("https://github.com", result);
-
-        List<String> list = Xsoup.compile("//tr/td/text()").evaluate(document).list();
-       // Assert.assertEquals("a", list.get(0));
-        //Assert.assertEquals("b", list.get(1));
-        int a = 2;
     }
 }
